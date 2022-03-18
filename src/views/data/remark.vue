1184a6ce3503849b6dc773d7113eb2d157bfb9b4
jslyzx <821203886@qq.com>
2022-03-18 20:23:03 +0800

    能效考评

--- src/views/data/remark.vue
+++ src/views/data/remark.vue
@@ -33,7 +33,7 @@
         <el-menu-item index="蒸汽">蒸汽</el-menu-item>
       </el-menu>
       <div class="chart-title">压力曲线</div>
-      <div ref="chart2" class="chart-con" />
+      <div ref="chart3" class="chart-con" />
       <div class="tip">
         <div class="tip-title">压力总体情况</div>
         <div class="tip-info">
@@ -67,7 +67,31 @@
             </div>
           </div>
         </el-col>
-        <el-col :span="12"></el-col>
+        <el-col :span="12">
+          <table cellspacing="0" class="table">
+            <tr>
+              <td>液氧单耗（t/Nm3）</td>
+              <td>498.55</td>
+            </tr>
+            <tr>
+              <td>二氧化碳单耗（t/Nm3）</td>
+              <td>319.31</td>
+            </tr>
+            <tr>
+              <td>天然气单耗（t/Nm3）</td>
+              <td>800.35</td>
+            </tr>
+            <tr>
+              <td>丙烷单耗（t/Nm3）</td>
+              <td>929.97</td>
+            </tr>
+            <tr>
+              <td>蒸汽单耗（t/Nm3）</td>
+              <td>655.20</td>
+            </tr>
+          </table>
+          <div class="tip">•建议关注用气规律，提升用气的平稳度。</div>
+        </el-col>
       </el-row>
     </div>
   </div>
@@ -120,6 +144,17 @@ export default {
       getGuanZhongData().then(response => {
         this.list = response.data
       })
+    },
+    randomData(now, value, oneDay) {
+      now = new Date(+now + oneDay);
+      value = value + Math.random() * 21 - 10;
+      return {
+        name: now.toString(),
+        value: [
+          [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
+          Math.round(value)
+        ]
+      };
     },
     initChart1() {
       this.chart1 = echarts.init(this.$refs.chart1, 'macarons')
@@ -184,6 +219,15 @@ export default {
     },
     initChart2() {
       this.chart2 = echarts.init(this.$refs.chart2, 'macarons')
+      let base = +new Date(1988, 9, 3);
+      let oneDay = 24 * 3600 * 1000;
+      let data = [
+        [base, Math.random() * 300]
+      ];
+      for (let i = 1; i < 20000; i++) {
+        let now = new Date((base += oneDay));
+        data.push([+now, Math.round((Math.random() - 0.5) * 20 + data[i - 1][1])]);
+      }
       this.chart2.setOption({
         grid: {
           left: '3%',
@@ -191,51 +235,89 @@ export default {
           bottom: '3%',
           containLabel: true
         },
-        legend: {},
-        xAxis: [{
-          type: 'category',
-          data: ['09-01', '09-02', '09-03', '09-04', '09-05', '09-06', '09-07', '09-08', '09-09', '09-10']
-        }],
-        yAxis: [{
-            type: 'value',
-            name: 't/m3',
-            min: 0,
-            max: 1000,
-            interval: 250
+        tooltip: {
+          trigger: 'axis',
+          position: function(pt) {
+            return [pt[0], '10%'];
+          }
+        },
+        xAxis: {
+          type: 'time',
+          boundaryGap: false
+        },
+        yAxis: {
+          type: 'value',
+          boundaryGap: [0, '100%']
+        },
+        dataZoom: [{
+            type: 'inside',
+            start: 0,
+            end: 20
           },
           {
-            type: 'value',
-            name: 't'
+            start: 0,
+            end: 20
           }
         ],
         series: [{
-            name: '本月产量',
-            type: 'bar',
-            data: this.generateRandomArray(0, 1000, 10)
-          }, {
-            name: '上月产量',
-            type: 'bar',
-            data: this.generateRandomArray(0, 1000, 10)
-          },
-          {
-            name: '本月用气量',
-            type: 'line',
-            yAxisIndex: 1,
-            data: this.generateRandomArray(0, 1000, 10),
-            symbol: 'none'
-          },
-          {
-            name: '上月用气量',
-            type: 'line',
-            yAxisIndex: 1,
-            data: this.generateRandomArray(0, 1000, 10),
-            symbol: 'none'
-          }
-        ]
+          type: 'line',
+          smooth: true,
+          symbol: 'none',
+          areaStyle: {},
+          data: data
+        }]
       })
     },
     initChart3() {
-
+      this.chart3 = echarts.init(this.$refs.chart3, 'macarons')
+      let base = +new Date(1988, 9, 3);
+      let oneDay = 24 * 3600 * 1000;
+      let data = [
+        [base, Math.random() * 300]
+      ];
+      for (let i = 1; i < 20000; i++) {
+        let now = new Date((base += oneDay));
+        data.push([+now, Math.round((Math.random() - 0.5) * 20 + data[i - 1][1])]);
+      }
+      this.chart3.setOption({
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        tooltip: {
+          trigger: 'axis',
+          position: function(pt) {
+            return [pt[0], '10%'];
+          }
+        },
+        xAxis: {
+          type: 'time',
+          boundaryGap: false
+        },
+        yAxis: {
+          type: 'value',
+          boundaryGap: [0, '100%']
+        },
+        dataZoom: [{
+            type: 'inside',
+            start: 0,
+            end: 20
+          },
+          {
+            start: 0,
+            end: 20
+          }
+        ],
+        series: [{
+          type: 'line',
+          smooth: true,
+          symbol: 'none',
+          areaStyle: {},
+          data: data
+        }]
+      })
     },
     initChart4() {
       this.chart4 = echarts.init(this.$refs.chart4, 'macarons')
@@ -380,6 +462,7 @@ export default {
 
 .total {
   background-color: #fff;
+  padding-bottom: 20px;
 
   .title {
     font-size: 14px;
@@ -422,13 +505,41 @@ export default {
       color: #FC642D;
       line-height: 170px;
       text-align: center;
-      span{
+
+      span {
         color: #494949;
         font-size: 14px;
         position: absolute;
         bottom: 0;
       }
     }
+  }
+
+  .table {
+    width: 80%;
+    margin-top: 30px;
+
+    td {
+      border: 1px solid #F0F0F0;
+      font-size: 12px;
+      line-height: 32px;
+      height: 32px;
+      text-align: left;
+      padding-left: 14px;
+      width: 50%;
+    }
+  }
+
+  .tip {
+    width: 80%;
+    height: 33px;
+    line-height: 33px;
+    background: #F2F2F2;
+    color: #494949;
+    border-radius: 17px;
+    font-size: 14px;
+    padding: 0 10px;
+    margin-top: 44px;
   }
 }
 
