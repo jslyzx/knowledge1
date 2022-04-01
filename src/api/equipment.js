74c3ebd6fec755031df6549395c17ac802c10aae
jslyzx <821203886@qq.com>
2022-04-01 22:12:41 +0800

    提交

--- src/api/equipment.js
+++ src/api/equipment.js
@@ -9,7 +9,7 @@ export function queryEquipmentSetting() {
 
 export function saveEquipmentSetting(data) {
   return request({
-    url: '../api/equipment/saveEquipmentSetting',
+    url: 'http://117.144.170.42:8089/prod-api/api/equipment/saveEquipmentSetting',
     method: 'post',
     data
   })
