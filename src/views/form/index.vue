3cc21503c36c8d41602bf37d710ef795fec6db28
jslyzx <821203886@qq.com>
2022-04-01 22:12:31 +0800

    提交

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -5,17 +5,17 @@
         <div class="part-title switchText">月产量信息录入</div>
         <el-form label-position="top" :model="form" size="small">
           <el-form-item label="万元产值外付动能：">
-            <el-input v-model="form.monthOutputValue" placeholder="请输入">
+            <el-input v-model.number="form.monthOutputValue" placeholder="请输入">
               <template slot="append">元/万元</template>
             </el-input>
           </el-form-item>
           <el-form-item label="吨投钢量动能：">
-            <el-input v-model="form.monthSteelQty" placeholder="请输入">
+            <el-input v-model.number="form.monthSteelQty" placeholder="请输入">
               <template slot="append">元/t</template>
             </el-input>
           </el-form-item>
           <el-form-item label="修正总吨动能：">
-            <el-input v-model="form.monthTotalQty" placeholder="请输入">
+            <el-input v-model.number="form.monthTotalQty" placeholder="请输入">
               <template slot="append">元/t</template>
             </el-input>
           </el-form-item>
@@ -25,17 +25,17 @@
         <div class="part-title switchText">日产量信息录入</div>
         <el-form label-position="top" :model="form" size="small">
           <el-form-item label="万元产值外付动能：">
-            <el-input v-model="form.dayOutputValue" placeholder="请输入">
+            <el-input v-model.number="form.dayOutputValue" placeholder="请输入">
               <template slot="append">元/万元</template>
             </el-input>
           </el-form-item>
           <el-form-item label="吨投钢量动能：">
-            <el-input v-model="form.daySteelQty" placeholder="请输入">
+            <el-input v-model.number="form.daySteelQty" placeholder="请输入">
               <template slot="append">元/t</template>
             </el-input>
           </el-form-item>
           <el-form-item label="修正总吨动能：">
-            <el-input v-model="form.dayTotalQty" placeholder="请输入">
+            <el-input v-model.number="form.dayTotalQty" placeholder="请输入">
               <template slot="append">元/t</template>
             </el-input>
           </el-form-item>
@@ -45,12 +45,12 @@
         <div class="part-title switchText">月产量信息录入</div>
         <el-form label-position="top" :model="form" size="small">
           <el-form-item label="当月用电量累积：">
-            <el-input v-model="form.totalElectricQty" placeholder="请输入">
+            <el-input v-model.number="form.totalElectricQty" placeholder="请输入">
               <template slot="append">kWh</template>
             </el-input>
           </el-form-item>
           <el-form-item label="当月压缩空气累积：">
-            <el-input v-model="form.totalAirQty" placeholder="请输入">
+            <el-input v-model.number="form.totalAirQty" placeholder="请输入">
               <template slot="append">Nm3</template>
             </el-input>
           </el-form-item>
@@ -62,27 +62,27 @@
         <div class="part-title switchText">单耗录入</div>
         <el-form label-position="top" :model="form" size="small">
           <el-form-item label="液氧单耗：">
-            <el-input v-model="form.yeyangQty" placeholder="请输入">
+            <el-input v-model.number="form.yeyangQty" placeholder="请输入">
               <template slot="append">t/Nm3：</template>
             </el-input>
           </el-form-item>
           <el-form-item label="二氧化碳单耗：">
-            <el-input v-model="form.coQty" placeholder="请输入">
+            <el-input v-model.number="form.coQty" placeholder="请输入">
               <template slot="append">t/Nm3：</template>
             </el-input>
           </el-form-item>
           <el-form-item label="天然气单耗：">
-            <el-input v-model="form.tianranqiQty" placeholder="请输入">
+            <el-input v-model.number="form.tianranqiQty" placeholder="请输入">
               <template slot="append">t/Nm3：</template>
             </el-input>
           </el-form-item>
           <el-form-item label="丙烷单耗：">
-            <el-input v-model="form.bianwanQty" placeholder="请输入">
+            <el-input v-model.number="form.bianwanQty" placeholder="请输入">
               <template slot="append">t/Nm3：</template>
             </el-input>
           </el-form-item>
           <el-form-item label="蒸汽单耗：">
-            <el-input v-model="form.zhenqiQty" placeholder="请输入">
+            <el-input v-model.number="form.zhenqiQty" placeholder="请输入">
               <template slot="append">t/Nm3：</template>
             </el-input>
           </el-form-item>
@@ -92,27 +92,27 @@
         <div class="part-title switchText">单价录入</div>
         <el-form label-position="top" :model="form" size="small">
           <el-form-item label="液氧单价：">
-            <el-input v-model="form.yeyangPrice" placeholder="请输入">
+            <el-input v-model.number="form.yeyangPrice" placeholder="请输入">
               <template slot="append">元/t</template>
             </el-input>
           </el-form-item>
           <el-form-item label="二氧化碳单价：">
-            <el-input v-model="form.coPrice" placeholder="请输入">
+            <el-input v-model.number="form.coPrice" placeholder="请输入">
               <template slot="append">元/t</template>
             </el-input>
           </el-form-item>
           <el-form-item label="天然气单价：">
-            <el-input v-model="form.tianranqiPrice" placeholder="请输入">
+            <el-input v-model.number="form.tianranqiPrice" placeholder="请输入">
               <template slot="append">元/t</template>
             </el-input>
           </el-form-item>
           <el-form-item label="丙烷单价：">
-            <el-input v-model="form.bianwanPrice" placeholder="请输入">
+            <el-input v-model.number="form.bianwanPrice" placeholder="请输入">
               <template slot="append">元/t</template>
             </el-input>
           </el-form-item>
           <el-form-item label="蒸汽单价：">
-            <el-input v-model="form.zhenqiPrice" placeholder="请输入">
+            <el-input v-model.number="form.zhenqiPrice" placeholder="请输入">
               <template slot="append">元/t</template>
             </el-input>
           </el-form-item>
@@ -123,7 +123,7 @@
       <el-col :span="6">
         <div class="item">
           <div class="item-t switchText">核心动力设备能耗</div>
-          <el-form size="small" v-model="form6" label-width="60px">
+          <el-form size="small" :model="form6" label-width="60px">
             <el-form-item label="年份：">
               <el-select v-model="form6.year">
                 <el-option label="2022" value="2022" />
@@ -213,7 +213,7 @@
       <el-col :span="6">
         <div class="item">
           <div class="item-t switchText">万修正吨</div>
-          <el-form size="small" v-model="form7" label-width="60px">
+          <el-form size="small" :model="form7" label-width="60px">
             <el-form-item label="年份：">
               <el-select v-model="form7.year">
                 <el-option label="2022" value="2022" />
@@ -303,7 +303,7 @@
       <el-col :span="6">
         <div class="item">
           <div class="item-t switchText">整体生产能耗</div>
-          <el-form size="small" v-model="form8" label-width="60px">
+          <el-form size="small" :model="form8" label-width="60px">
             <el-form-item label="年份：">
               <el-select v-model="form8.year">
                 <el-option label="2022" value="2022" />
